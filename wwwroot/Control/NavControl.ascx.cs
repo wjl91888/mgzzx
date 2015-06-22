9ac7cc32e4f87630802a4f8a29236bb39920fb3a
Rocky Wang <wjl91888@hotmail.com>
2015-06-23 07:43:55 +0800

    111

---
+++ wwwroot/Control/NavControl.ascx.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Web.UI.WebControls;
+using RICH.Common;
+using RICH.Common.LM;
+using RICH.Common.PM;
+using Telerik.Web.UI;
+
+public partial class NavControl : System.Web.UI.UserControl
+{
+    private Hashtable htInputParameter;
+    private Hashtable htOutputParameter;
+    private DataSet dsRecordInfo;
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        linkDefaultIndex.HRef = ConstantsManager.DEFAULT_ADMINISTRATOR_INDEX;
+    }
+
+    public void GetMenu(string groupID)
+    {
+        htInputParameter = new Hashtable();
+        dsRecordInfo = new DataSet();
+        htInputParameter.Add(ConstantsManager.QUERY_DATASET_NAME, dsRecordInfo);
+        htInputParameter.Add(ConstantsManager.MESSAGE_ID, "");
+        htInputParameter.Add("UserID", Session["UserID"]);
+        htInputParameter.Add("IsPageMenu", "true");
+        htInputParameter.Add("PurviewPRI", 1);
+
+        UserPurviewLibrary instanceUserPurviewLibrary = new UserPurviewLibrary();
+        htOutputParameter = instanceUserPurviewLibrary.GetUserPurviewInfoForMenu(htInputParameter);
+        dsRecordInfo = (DataSet)htOutputParameter[ConstantsManager.QUERY_DATASET_NAME];
+
+        //rtvMenu.DataNavigateUrlField = "PageFileName";
+        //rtvMenu.DataFieldParentID = "PurviewTypeID";
+        //rtvMenu.DataFieldID = "PurviewID";
+        //rtvMenu.DataTextField = "PurviewName";
+        //rtvMenu.DataValueField = "PurviewID";
+        dsRecordInfo.Tables[0].DefaultView.RowFilter = "UserGroupID='{0}'".FormatInvariantCulture(groupID);
+        NavList.DataSource = dsRecordInfo.Tables[0].DefaultView;
+        NavList.DataBind();
+    }
+
+}
--- wwwroot/MasterPages/NewBasePage.master.cs
+++ wwwroot/MasterPages/NewBasePage.master.cs
@@ -12,24 +12,6 @@ using Telerik.Web.UI;
 
 public partial class MasterPages_NewBasePage : System.Web.UI.MasterPage
 {
-    #region 当前页面变量定义
-    /// <summary>
-    /// 输入参数HashTable
-    /// </summary>
-    private Hashtable htInputParameter;
-
-    /// <summary>
-    /// 输出参数HashTable
-    /// </summary>
-    private Hashtable htOutputParameter;
-
-    /// <summary>
-    /// 用来获取返回查询结果或者作为批量数据的输入的DataSet
-    /// </summary>
-    private DataSet dsRecordInfo;
-
-    #endregion
-
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -46,7 +28,6 @@ public partial class MasterPages_NewBasePage : System.Web.UI.MasterPage
 
     private void InitalPage()
     {
-        linkDefaultIndex.HRef = ConstantsManager.DEFAULT_ADMINISTRATOR_INDEX;
         if (!DataValidateManager.ValidateIsNull(Session[ConstantsManager.SESSION_USER_GROUP_ID]))
         {
             string[] strArrUserGroupID = ((string)Session[ConstantsManager.SESSION_USER_GROUP_ID]).Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
@@ -57,51 +38,15 @@ public partial class MasterPages_NewBasePage : System.Web.UI.MasterPage
                 strUserGroupName = (string)t_PM_UserGroupInfoBusinessEntityBase.GetValueByFixCondition("UserGroupID", strUserGroupID, "UserGroupName");
                 if (!DataValidateManager.ValidateIsNull(strUserGroupName))
                 {
-                    ddlUserGroupID.Items.Add(new RadComboBoxItem(strUserGroupName, strUserGroupID));
+                    ddlUserGroupID.Items.Add(new ListItem(strUserGroupName, strUserGroupID));
                 }
             }
         }
-    }
-
-    private void GetMenu()
-    {
-        htInputParameter = new Hashtable();
-        dsRecordInfo = new DataSet();
-        htInputParameter.Add(ConstantsManager.QUERY_DATASET_NAME, dsRecordInfo);
-        htInputParameter.Add(ConstantsManager.MESSAGE_ID, "");
-        htInputParameter.Add("UserID", Session["UserID"]);
-        htInputParameter.Add("IsPageMenu", "true");
-        htInputParameter.Add("PurviewPRI", 1);
-
-        UserPurviewLibrary instanceUserPurviewLibrary = new UserPurviewLibrary();
-        htOutputParameter = instanceUserPurviewLibrary.GetUserPurviewInfoForMenu(htInputParameter);
-        dsRecordInfo = (DataSet)htOutputParameter[ConstantsManager.QUERY_DATASET_NAME];
-
-        rtvMenu.DataNavigateUrlField = "PageFileName";
-        rtvMenu.DataFieldParentID = "PurviewTypeID";
-        rtvMenu.DataFieldID = "PurviewID";
-        rtvMenu.DataTextField = "PurviewName";
-        rtvMenu.DataValueField = "PurviewID";
-        dsRecordInfo.Tables[0].DefaultView.RowFilter = "UserGroupID='{0}'".FormatInvariantCulture(ddlUserGroupID.SelectedValue);
-        rtvMenu.DataSource = dsRecordInfo.Tables[0].DefaultView;
-        rtvMenu.DataBind();
-        rtvMenu.ExpandAllNodes();
+        NavList.GetMenu(ddlUserGroupID.SelectedValue);
     }
 
     protected void ddlUserGroupID_OnSelectedIndexChanged(object sender, EventArgs e)
     {
-        GetMenu();
+        NavList.GetMenu(ddlUserGroupID.SelectedValue);
     }
-
-    protected void rtvMenu_NodeDataBound(object sender, RadTreeNodeEventArgs e)
-    {
-        var node = e.Node;
-        if (!node.NavigateUrl.IsHtmlNullOrWiteSpace())
-        {
-            var dr = (DataRowView)node.DataItem;
-            node.Target = "ContentFrame";
-            node.NavigateUrl = "..{0}/{1}".FormatInvariantCulture(dr["PageFilePath"], dr["PageFileName"]);
-        }
-    }
-
 }
